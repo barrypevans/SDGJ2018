fe20a78db0cfa6e9a394f0219e83cab494b0db4b
pbracco04@aol.com <franchesca1>
2018-09-29 12:25:41 -0700

    + Added pause menu + Updated button assets

---
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+    public EventSystem EventSystem;
+    public Menu_Pause Menu_Pause;
+    public BeatManager BeatManager;
+    private bool m_bIsPaused;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Options"))
+        {
+            if (m_bIsPaused)
+            {
+                this.UnPauseGame();
+            }
+            else
+            {
+                this.PauseGame();
+            }
+        }
+		
+	}
+
+    public void PauseGame()
+    {
+        m_bIsPaused = true;
+        Time.timeScale = 0.0f;
+        if (BeatManager!= null)
+        {
+            BeatManager.GetComponent<AudioSource>().Pause();
+        }
+        if (Menu_Pause != null)
+        {
+            Menu_Pause.gameObject.SetActive(true);
+            if (EventSystem != null)
+            {
+                Menu_Pause.FirstSelectable.Select();
+            }
+        }
+    }
+
+    public void UnPauseGame()
+    {
+        m_bIsPaused = false;
+        Time.timeScale = 1.0f;
+        if (BeatManager != null)
+        {
+            BeatManager.GetComponent<AudioSource>().UnPause();
+        }
+        if (Menu_Pause != null)
+        {
+            Menu_Pause.gameObject.SetActive(false);
+            if (EventSystem != null)
+            {
+                EventSystem.SetSelectedGameObject(null);
+            }
+        }
+    }
+}
