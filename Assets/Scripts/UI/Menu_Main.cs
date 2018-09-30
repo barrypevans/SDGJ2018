6ea18c609743445e7858d1f32300ce98cd46eef1
pbracco04@aol.com <franchesca1>
2018-09-29 23:29:10 -0700

    + Updated main and credits menus

--- Assets/Scripts/UI/Menu_Main.cs
+++ Assets/Scripts/UI/Menu_Main.cs
@@ -6,13 +6,6 @@ using UnityEngine.UI;
 
 public class Menu_Main : MonoBehaviour {
 
-    public GameObject Title;
-    public GameObject StartGame;
-    public GameObject Credits;
-    public GameObject Quit;
-    public float TimeTitleOnScreen;
-    private float m_fTotalElapsedTime = 0.0f;
-
     // Use this for initialization
     void Start () {
 		
@@ -20,19 +13,6 @@ public class Menu_Main : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (m_fTotalElapsedTime < TimeTitleOnScreen)
-        {
-            m_fTotalElapsedTime += Time.deltaTime;
-
-            if (m_fTotalElapsedTime >= TimeTitleOnScreen)
-            {
-                Title.SetActive(false);
-                StartGame.SetActive(true);
-                StartGame.GetComponent<Selectable>().Select();
-                Credits.SetActive(true);
-                Quit.SetActive(true);
-            }
-        }
 	}
 
     public void OnClick_StartGame()
