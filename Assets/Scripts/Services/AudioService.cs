fdfea71cf37aef3bdc679bd20a26558a4b97e29a
Barry <barry.p.evans@gmail.com>
2018-09-29 18:57:30 -0700

    [+] Audio Service

---
+++ Assets/Scripts/Services/AudioService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioService : MonoBehaviour
+{
+    static AudioService Instance;
+
+    private void Awake()
+    {
+        if (null == Instance)
+            Instance = this;
+        else
+        {
+            Destroy(this);
+        }
+
+    }
+
+}
